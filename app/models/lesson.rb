dea2857a4f6f973ab1cebf44c4b6f3ed935a1e03
marco ranieri <mr.marcoranieri@gmail.com>
2018-08-29 21:52:43 +0200

    Fixed favourite student/tutor (current_user.lesson)

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -14,20 +14,4 @@ class Lesson < ApplicationRecord
   # MoneyRails Gem
   monetize :price_cents
 
-  # Adding to favourite when a tutor click CONFIRM btn
-  def add_tutor_to_favourite(student, lesson_id)
-    # |= Appending Unique value
-    student.mytutor_ids |= [lesson_id]
-    student.save!
-  end
-  # Adding to favourite when a tutor click CONFIRM btn
-  def add_student_to_favourite(tutor, lesson_id)
-    # |= Appending Unique value
-    tutor.mystudent_ids |= [lesson_id]
-    tutor.save!
-  end
-end
-
-def find_lesson(lesson_id)
-  Lesson.find_by_id(lesson_id)
 end
